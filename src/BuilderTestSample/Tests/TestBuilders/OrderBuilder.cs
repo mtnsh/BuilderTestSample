02119e324ed6cfb179d04f089df052a3a5fc5f6b
Matan Shlomo <Matan.Shlomo@votiro.com>
2022-02-19 12:54:21 +0200

    replace next lines with a CustomerBuilder you create

--- src/BuilderTestSample/Tests/TestBuilders/OrderBuilder.cs
+++ src/BuilderTestSample/Tests/TestBuilders/OrderBuilder.cs
@@ -9,15 +9,14 @@ namespace BuilderTestSample.Tests.TestBuilders
     public class OrderBuilder
     {
         private readonly Order _order = new ();
+        private readonly CustomerBuilder _customerBuilder = new();
+        private readonly AddressBuilder _addressBuilder = new();
 
         public OrderBuilder()
         {
             _order.Id = 0;
             _order.TotalAmount = 100m;
-
-            // TODO: replace next lines with a CustomerBuilder you create
-            // _order.Customer = new Customer();
-            // _order.Customer.HomeAddress = new Address();
+            _order.Customer = _customerBuilder.Build();
         }
 
         public OrderBuilder WithId(int id)
